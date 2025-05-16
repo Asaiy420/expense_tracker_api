f134079d658e671eabbeeb8be69e762ebfd9215a
Asaiy420 <kcaryan05@gmail.com>
2025-05-16 17:50:09 +0545

    added cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
-import { PrismaClient } from "../generated/prisma";
 import "dotenv/config";
 import authRoute from "./routes/auth.route";
+import cookieParser from "cookie-parser";
 
 const app = express();
-const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoute)
 
 app.listen(PORT, () => {
